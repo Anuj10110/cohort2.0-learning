e1f066da2face2219ede24a79f82a42a51e27be2
Anuj10110 <anuj.10110.taneja@gmail.com>
2026-01-16 11:12:46 +0530

    Task 13 Competed - Created an IPL Winner Guesser

--- script.js
+++ script.js
@@ -1,38 +1,99 @@
-let stories = [
-    {
-        dp : "https://i.pinimg.com/736x/eb/76/a4/eb76a46ab920d056b02d203ca95e9a22.jpg",
-        story : "https://i.pinimg.com/736x/96/61/ea/9661eaa55b90fc899b0f642054e00dd2.jpg",
-    },
-    {
-        dp : "https://i.pinimg.com/736x/72/1d/95/721d957dcbc1ea47675146260c3b41d7.jpg",
-        story : "https://i.pinimg.com/736x/dc/69/35/dc6935b674e4777f54cd6f4fa83aa840.jpg",
-    },
-    {
-        dp : "https://i.pinimg.com/736x/e6/d4/79/e6d4799474ae8fca2f13c16a200e96d1.jpg",
-        story : "https://i.pinimg.com/736x/88/b0/27/88b02718e21047d8c397a8d97be6055d.jpg",
-    },
-    {
-        dp : "https://i.pinimg.com/736x/f2/42/97/f242977db5d5e96972444d9013b37bea.jpg",
-        story : "https://i.pinimg.com/736x/87/b1/7f/87b17fb4f6d602cf2606fdf482e41c2b.jpg",
-    },
-    {
-        dp : "https://i.pinimg.com/736x/eb/bc/9e/ebbc9ef623a94299c760105ae27979cc.jpg",
-        story : "https://i.pinimg.com/1200x/67/ae/72/67ae7245af90c40886c040b27e18a95d.jpg",
-    }
-]
-clutter = ""
-let profile = document.querySelector("main #full-screen #story")
-stories.forEach(function(val, idx) {
-    clutter += `<div class="card">
-                    <img src="${val.dp}" alt="" id="${idx}">
-                </div>`
-})
-profile.innerHTML = clutter
-profile.addEventListener("click", function(details) {
-    document.querySelector("main #full-screen #display-story").style.display = "block"
-    document.querySelector("main #full-screen #display-story").style.backgroundImage = `url(${stories[details.target.id].story})`
+let card = document.querySelector('main section .card');
+let button = document.querySelector('main section button');
 
-    setTimeout(function() {
-        document.querySelector("main #full-screen #display-story").style.display = "none"
-    },2500)
+let arr = [
+    {
+        Team : 'RCB',
+        Captain : 'Rajat Patidar',
+        Trophies : 1,
+        Years_Won : [2025],
+        PrimaryColor : '#7a2d34ff',
+        SecondaryColor : '#C2A34E'
+    },
+    {
+        Team : 'PBKS',
+        Captain : 'Shreyas Iyer',
+        Trophies : 0,
+        Years_Won : [],
+        PrimaryColor : '#D7181F',
+        SecondaryColor : '#FBFBFA'
+    },
+    {
+        Team : 'RR',
+        Captain : 'Sanju Samson',
+        Trophies : 1,
+        Years_Won : [2008],
+        PrimaryColor : '#EB82B5',
+        SecondaryColor : '#2E3D9A'
+    },
+    {
+        Team : 'MI',
+        Captain : 'Hardik Pandya',
+        Trophies : 5,
+        Years_Won : [2013, 2015, 2017, 2019, 2020],
+        PrimaryColor : '#2E6AB1',
+        SecondaryColor : '#FAD230'
+    },
+    {
+        Team : 'CSK',
+        Captain : 'MS Dhoni',
+        Trophies : 5,
+        Years_Won : [2010, 2011, 2018, 2021, 2023],
+        PrimaryColor : '#ffcb03',
+        SecondaryColor : '#c37e06ff'
+    },
+    {
+        Team : 'KKR',
+        Captain : 'Ajinkya Rahane',
+        Trophies : 3,
+        Years_Won : [2012, 2014, 2024],
+        PrimaryColor : '#28204A',
+        SecondaryColor : '#FEF0AE'
+    },
+    {
+        Team : 'GT',
+        Captain : 'Shubman Gill',
+        Trophies : 1,
+        Years_Won : [2022],
+        PrimaryColor : '#3a667dff',
+        SecondaryColor : '#cfaf56ff'
+    },
+    {
+        Team : 'LSG',
+        Captain : 'Rishabh Pant',
+        Trophies : 0,
+        Years_Won : [],
+        PrimaryColor : '#0648BC',
+        SecondaryColor : '#FFFFFF'
+    },
+    {
+        Team : 'SRH',
+        Captain : 'Pat Cummins',
+        Trophies : 1,
+        Years_Won : [2016],
+        PrimaryColor : '#F26522',
+        SecondaryColor : '#664b20ff'
+    },
+    {
+        Team : 'DC',
+        Captain : 'Axar Patel',
+        Trophies : 0,
+        Years_Won : [],
+        PrimaryColor : '#6c2926ff',
+        SecondaryColor : '#5f86c5ff'
+    },
+]
+
+
+button.addEventListener('click', function () {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    card.innerHTML = `<h1>
+                        Team: ${arr[randomIndex].Team} <br>
+                        Captain: ${arr[randomIndex].Captain} <br>
+                        Trophies : ${arr[randomIndex].Trophies} <br>
+                        Years Won: ${arr[randomIndex].Years_Won}
+                    </h1>`
+    card.style.backgroundColor = arr[randomIndex].PrimaryColor;
+    card.style.color = arr[randomIndex].SecondaryColor;
+    
 })
