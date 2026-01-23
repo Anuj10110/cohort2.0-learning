7f8bf3746cc9b73787e424ff43f12c9eee0602f0
Anuj10110 <anuj.10110.taneja@gmail.com>
2026-01-23 15:04:48 +0530

    Task 25 Completed

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-addEventListener('mousemove', function(dets) {
-    document.body.style.setProperty('--x', dets.x + 'px')
-    document.body.style.setProperty('--y', dets.y + 'px')
+const h1 = document.querySelector('p');
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text =h1.innerText; 
+
+let iteration = 0;
+
+function randomText () {
+    const str = text.split('').map(function (char, idx) {
+        if (idx < iteration) {
+            return char;
+        }
+        return characters.split('')[Math.floor(Math.random() * 52)];
+    }).join('');
+    h1.innerText = str;
+    iteration += 0.25;
+}
+
+
+h1.addEventListener('mouseenter', function () {
+    iteration = 0;
+    setInterval(() => {
+        randomText()
+    }, 50);
 })
